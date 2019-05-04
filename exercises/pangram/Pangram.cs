a502661b58831ec4fd81b390a557f1d6479f47f8
martinvds <martinvds@gmail.com>
2019-05-04 12:43:24 +0200

    Added Pangram

--- exercises/pangram/Pangram.cs
+++ exercises/pangram/Pangram.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Linq;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        var charsGrouped = from l in input.ToLowerInvariant().ToCharArray()
+                           where char.IsLetter(l)
+                           group l by l into grp
+                           select grp.Count();
+
+        return charsGrouped.Count() == 26;
     }
 }
