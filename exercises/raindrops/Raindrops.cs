380301a75a5b74e217b9b14754fb7edcb6fc1405
martinvds <martinvds@gmail.com>
2019-05-04 12:40:23 +0200

    Added Raindrops

--- exercises/raindrops/Raindrops.cs
+++ exercises/raindrops/Raindrops.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Linq;
 
 public static class Raindrops
 {
+    enum Drops
+    {
+        Pling = 3,
+        Plang = 5,
+        Plong = 7
+    }
+
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var factors = Enumerable.Range(1, number).Where(x => number % x == 0 && IsValidFactor(x));
+
+        if (!factors.Any())
+        {
+            return number.ToString();
+        }
+
+        return string.Join(string.Empty, factors.Select(f => Enum.GetName(typeof(Drops), f)));
     }
+
+    static bool IsValidFactor(int factor) => factor == (int)Drops.Pling || factor == (int)Drops.Plang || factor == (int)Drops.Plong;
 }
