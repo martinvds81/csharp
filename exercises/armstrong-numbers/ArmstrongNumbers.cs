abc747bae4d9f89cca859887c04b699b81561e57
martinvds <martinvds@gmail.com>
2019-05-04 12:37:25 +0200

    Added ArmstrongNumbers

--- exercises/armstrong-numbers/ArmstrongNumbers.cs
+++ exercises/armstrong-numbers/ArmstrongNumbers.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var digits = number.GetDigits().Reverse().ToList();
+        var power = digits.Count();
+
+        return Enumerable.Range(0, power).Sum(x => Math.Pow(digits[x], power)) == number;
+    }
+
+    public static IEnumerable<int> GetDigits(this int number)
+    {
+        while (number > 0)
+        {
+            var digit = number % 10;
+            number /= 10;
+            yield return digit;
+        }
     }
 }
