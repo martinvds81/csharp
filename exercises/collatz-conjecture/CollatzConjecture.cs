0a933f3ef4dc108fce9de90ae2fcaa9ae54ef39b
martinvds <martinvds@gmail.com>
2019-05-04 12:37:39 +0200

    Added CollatzConjecture

--- exercises/collatz-conjecture/CollatzConjecture.cs
+++ exercises/collatz-conjecture/CollatzConjecture.cs
@@ -4,6 +4,22 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        if(number <= 0)
+        {
+            throw new ArgumentException("No negative numbers");
+        }
+
+        int stepCounter = 0;
+
+        while (number != 1)
+        {
+            number = number.IsEven() ? number / 2 : number * 3 + 1;
+
+            stepCounter++;
+        }
+
+        return stepCounter;
     }
+
+    static bool IsEven(this int number) => number % 2 == 0;
 }
