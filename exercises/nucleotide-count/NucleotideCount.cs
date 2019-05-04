bb3a013452643dfbe1c5564a9df9ce03f13e21e5
martinvds <martinvds@gmail.com>
2019-05-04 12:42:40 +0200

    Added NucleotideCount

--- exercises/nucleotide-count/NucleotideCount.cs
+++ exercises/nucleotide-count/NucleotideCount.cs
@@ -1,10 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class NucleotideCount
 {
+    private const string validNucleotide = "ACGT";
+
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!sequence.ToCharArray().All(validNucleotide.Contains))
+        {
+            throw new ArgumentException("Invalid nucleotides");
+        }
+
+        return (from s in string.Concat(sequence, validNucleotide).ToCharArray()
+                group s by s into g
+                orderby g.Key
+                select new { key = g.Key, value = g.Count() - 1 }).ToDictionary(k => k.key, v => v.value);
     }
 }
