0cc1eea8a927b531fc850521ce847ded66c63e88
martinvds <martinvds@gmail.com>
2019-05-04 12:45:36 +0200

    Added RobotName

--- exercises/robot-name/RobotName.cs
+++ exercises/robot-name/RobotName.cs
@@ -1,17 +1,55 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 public class Robot
 {
-    public string Name
+    private static HashSet<string> _nameHistory = new HashSet<string>();
+    private readonly Random _random = new Random();
+
+    public Robot()
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        Reset();
     }
+
+    public string Name { get; private set; }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var robotName = GenerateRobotName();
+        int max = 0;
+        while (_nameHistory.Contains(robotName))
+        {
+            robotName = GenerateRobotName();
+
+            max++;
+
+            if (max >= 10000)
+            {
+                throw new ArgumentException("The robots are coming!");
+            }
+        }
+
+        _nameHistory.Add(robotName);
+        Name = robotName;
+    }
+
+    public string GenerateRobotName()
+    {
+        var builder = new StringBuilder();
+        builder.Append(RandomString(2));
+        builder.Append(RandomNumber());
+        return builder.ToString();
+    }
+
+    public string RandomString(int length)
+    {
+        return new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ", length).Select(s => s[_random.Next(s.Length)]).ToArray());
+    }
+
+    public string RandomNumber()
+    {
+        return _random.Next(0, 999).ToString("000");
     }
 }
