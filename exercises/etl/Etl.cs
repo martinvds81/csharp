e667aded81811b1a010d2aec73f4ca2f8377fb50
martinvds <martinvds@gmail.com>
2019-05-04 12:45:23 +0200

    Added Etl

--- exercises/etl/Etl.cs
+++ exercises/etl/Etl.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 public static class Etl
 {
     public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return old
+              .SelectMany(o => o.Value.Select(p => new { key = p, value = o.Key }))
+              .ToDictionary(k => k.key.ToLower(), v => v.value);
+
     }
 }
