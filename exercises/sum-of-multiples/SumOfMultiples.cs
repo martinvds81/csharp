b393f0432909ecb4343c857a510b585c17c3a1bf
martinvds <martinvds@gmail.com>
2019-05-04 12:39:20 +0200

    Added SumOfMultiples

--- exercises/sum-of-multiples/SumOfMultiples.cs
+++ exercises/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var sumTotal = new HashSet<ulong>();
+
+        var x = 1_999_999_999UL + 1_999_999_999UL;
+
+        foreach (int multiple in multiples.Where(m => m <= max && m > 0))
+        {
+            ulong total = 0;
+
+            while (total + (ulong)multiple < (ulong)max)
+            {
+                total += (ulong)multiple;
+                sumTotal.Add(total);
+            }
+        }
+
+        return (int)sumTotal.DefaultIfEmpty().Aggregate((a, b) => a + b);
     }
 }
