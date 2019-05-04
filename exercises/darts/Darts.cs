523e1a1d520e21fb067d5847fe238e547ddc62ed
martinvds <martinvds@gmail.com>
2019-05-04 12:37:52 +0200

    Added Darts

--- exercises/darts/Darts.cs
+++ exercises/darts/Darts.cs
@@ -4,6 +4,7 @@ public static class Darts
 {
     public static int Score(double x, double y)
     {
-       throw new NotImplementedException("You need to implement this function");
+        var distance = Math.Sqrt(x * x + y * y);
+        return distance <= 1 ? 10 : distance <= 5 ? 5 : distance <= 10 ? 1 : 0;
     }
 }
