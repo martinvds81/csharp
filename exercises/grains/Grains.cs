9b984ea24520798e3232ebfff7436635714dc223
martinvds <martinvds@gmail.com>
2019-05-04 12:38:55 +0200

    Added Grains

--- exercises/grains/Grains.cs
+++ exercises/grains/Grains.cs
@@ -2,13 +2,40 @@
 
 public static class Grains
 {
+    /// <summary>
+    /// Calculate the number of grains of wheat on a chessboard given that the number on each square doubles.
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
     public static ulong Square(int n)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // 1 = 1
+        // 2 = 2
+        // 3 = 4
+        // 4 = 8
+        // 5 = 16
+
+        if(n <= 0)
+        {
+            throw new ArgumentOutOfRangeException("no negative numbers");
+        }
+        if (n > 64)
+        {
+            throw new ArgumentOutOfRangeException("number of squares is bigger then 64");
+        }
+
+        return (ulong)Math.Pow(2, n - 1);
     }
 
     public static ulong Total()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        ulong total = 0UL;
+
+        for (int i = 1; i < 65; i++)
+        {
+            total += Square(i);
+        }
+
+        return total;
     }
 }
