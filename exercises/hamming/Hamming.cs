d2d14242c1ea014fdca81611e6b437593299fa25
martinvds <martinvds@gmail.com>
2019-05-04 12:39:59 +0200

    Added Hamming

--- exercises/hamming/Hamming.cs
+++ exercises/hamming/Hamming.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Linq;
 
 public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!strandsAreComparable(firstStrand, secondStrand))
+        {
+            throw new ArgumentException("Strands are not comparable!");
+        }
+
+        return Enumerable.Range(0, firstStrand.Length).Count(s => firstStrand[s] != secondStrand[s]);
     }
+
+    static bool strandsAreComparable(string firstStrand, string secondStrand) => firstStrand.Length == secondStrand.Length;
 }
