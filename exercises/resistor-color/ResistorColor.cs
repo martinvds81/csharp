ce7d16c99a810cc0b0cce5fea899f3e3d0212408
martinvds <martinvds@gmail.com>
2019-05-04 12:41:41 +0200

    Added ResistorColor

--- exercises/resistor-color/ResistorColor.cs
+++ exercises/resistor-color/ResistorColor.cs
@@ -1,14 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class ResistorColor
 {
+    private static readonly IDictionary<string, int> ResistorColors = new Dictionary<string, int>()
+    {
+        { "black", 0 },
+        { "brown", 1 },
+        { "red",  2 },
+        { "orange", 3 },
+        { "yellow", 4 },
+        { "green", 5 },
+        { "blue", 6 },
+        { "violet", 7 },
+        { "grey", 8 },
+        { "white", 9 }
+    };
+
     public static int ColorCode(string color)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (ResistorColors.TryGetValue(color.ToLower(), out var resistorColor))
+        {
+            return resistorColor;
+        }
+        else
+        {
+            throw new ArgumentException("color is not defined");
+        }
     }
 
-    public static string[] Colors()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static string[] Colors() => ResistorColors.Keys.ToArray();
 }
--- exercises/resistor-color/ResistorColorTest.cs
+++ exercises/resistor-color/ResistorColorTest.cs
@@ -7,22 +7,22 @@ public class ResistorColorTest
     [Fact]
     public void Black()
     {
-        Assert.Equal(0, ResistorColor.ColorCode("black"));
+        Assert.Equal(0, ResistorColor.ColorCode("Black"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact()]
     public void White()
     {
         Assert.Equal(9, ResistorColor.ColorCode("white"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact()]
     public void Orange()
     {
         Assert.Equal(3, ResistorColor.ColorCode("orange"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact()]
     public void Colors()
     {
         Assert.Equal(new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" }, ResistorColor.Colors());
