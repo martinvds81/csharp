5120c2075679e326a0bcd784288d69920d51896d
martinvds <martinvds@gmail.com>
2019-05-04 12:43:42 +0200

    Added ProteinTranslation

--- exercises/protein-translation/ProteinTranslation.cs
+++ exercises/protein-translation/ProteinTranslation.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class ProteinTranslation
 {
+    private static IDictionary<string, string[]> ProteinList = new Dictionary<string, string[]>()
+    {
+        ["Methionine"] = new string[] { "AUG" },
+        ["Phenylalanine"] = new string[] { "UUU", "UUC" },
+        ["Leucine"] = new string[] { "UUA", "UUG" },
+        ["Serine"] = new string[] { "UCU", "UCC", "UCA", "UCG" },
+        ["Tyrosine"] = new string[] { "UAU", "UAC" },
+        ["Cysteine"] = new string[] { "UGU", "UGC" },
+        ["Tryptophan"] = new string[] { "UGG" }
+    };
+
+    private static string[] Stop = new string[] { "UAA", "UAG", "UGA" };
+
     public static string[] Proteins(string strand)
     {
-        throw new NotImplementedException();
+        // Get valid codons
+        var codons = Enumerable.Range(0, strand.Length / 3).Select(s => strand.Substring(s * 3, 3)).TakeWhile(s => !Stop.Contains(s));
+
+        var proteins = ProteinList.SelectMany(s => s.Value.Select(t => new { protein = s.Key, codon = t }));
+
+        return (from c in codons
+                join p in proteins on c equals p.codon
+                select p.protein).ToArray();
     }
 }
