d7638b5c78dee1d21b850000289a91a4a7b75a97
martinvds <martinvds@gmail.com>
2019-05-12 12:13:15 +0200

    Added RotationalCipher

--- exercises/rotational-cipher/RotationalCipher.cs
+++ exercises/rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Text;
 
 public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (string.IsNullOrEmpty(text))
+        {
+            throw new ArgumentException(nameof(text));
+        }
+
+        var builder = new StringBuilder();
+
+        foreach (var character in text)
+        {
+            // If it's not a letter just add it
+            if (!char.IsLetter(character))
+            {
+                builder.Append(character);
+            }
+            else
+            {
+                char d = char.IsUpper(character) ? 'A' : 'a';
+
+                var newCharacter = character + shiftKey;
+
+                if(newCharacter >= d + 26)
+                {
+                    newCharacter -= 26;
+                }
+
+                builder.Append((char)newCharacter);
+            }
+        }
+
+        return builder.ToString();
     }
 }
