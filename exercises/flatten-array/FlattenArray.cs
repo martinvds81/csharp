ba27e417ba0b3ef9288b17aefc8d43282ac0b5d7
martinvds <martinvds@gmail.com>
2019-05-12 12:08:21 +0200

    Added FlattenArray

--- exercises/flatten-array/FlattenArray.cs
+++ exercises/flatten-array/FlattenArray.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 
 public static class FlattenArray
 {
     public static IEnumerable Flatten(IEnumerable input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        foreach (var item in input)
+        {
+            if (item != null)
+            {
+                if (item is IEnumerable)
+                {
+                    foreach(var child in Flatten((IEnumerable)item))
+                    {
+                        yield return child;
+                    }
+                }
+                else
+                {
+                    yield return item;
+                }
+            }
+        }
     }
 }
