1d602c0a71a427fae216f387293f32bf5500b7f6
martinvds <martinvds@gmail.com>
2019-05-12 12:05:19 +0200

    Added Allegies

--- exercises/allergies/Allergies.cs
+++ exercises/allergies/Allergies.cs
@@ -1,30 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public enum Allergen
 {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
 }
 
 public class Allergies
 {
-    public Allergies(int mask)
-    {
-    }
+    private Allergen Mask { get; }
 
-    public bool IsAllergicTo(Allergen allergen)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public Allergies(int mask) => Mask = (Allergen)mask;
+
+    public bool IsAllergicTo(Allergen allergen) => Mask.HasFlag(allergen);
 
     public Allergen[] List()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Enum.GetValues(typeof(Allergen))
+             .Cast<Allergen>()
+             .Where(a => IsAllergicTo(a))
+             .ToArray();
     }
 }
