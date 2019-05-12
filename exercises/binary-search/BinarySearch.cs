13282f75f43afb2995fb33cfb23946e48e7fb4f0
martinvds <martinvds@gmail.com>
2019-05-12 12:05:56 +0200

    Added BinarySearch

--- exercises/binary-search/BinarySearch.cs
+++ exercises/binary-search/BinarySearch.cs
@@ -1,9 +1,27 @@
-﻿using System;
-
-public static class BinarySearch
+﻿public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int left = 0;
+        int right = input.Length - 1;
+
+        while (left <= right)
+        {
+            var middle = (left + right) / 2;
+            if (input[middle] < value)
+            {
+                left = middle + 1;
+            }
+            else if (input[middle] > value)
+            {
+                right = middle - 1;
+            }
+            else
+            {
+                return middle;
+            }
+        }
+
+        return -1;
     }
 }
