949bd952d81d8e577b6525651e364c7fe2b01120
martinvds <martinvds@gmail.com>
2019-05-12 12:11:01 +0200

    Added PhoneNumber

--- exercises/phone-number/PhoneNumber.cs
+++ exercises/phone-number/PhoneNumber.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Text.RegularExpressions;
 
 public class PhoneNumber
 {
     public static string Clean(string phoneNumber)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // First remove all non digits
+        phoneNumber = Regex.Replace(phoneNumber, @"[^\d]", "");
+
+        // Validate the cleandup phone number
+        var match = Regex.Match(phoneNumber, "(^1?)(?<valid>[2-9]{3}[2-9]{1}[0-9]{3}[0-9]{3})$");
+        if(!match.Success)
+        {
+            throw new ArgumentException("Invalid number");
+        }
+
+        return match.Groups["valid"].Value;
     }
 }
