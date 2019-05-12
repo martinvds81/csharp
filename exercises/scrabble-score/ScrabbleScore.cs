a02b9f7e19c69cbcd108a397c861b42af0b9cc85
martinvds <martinvds@gmail.com>
2019-05-12 12:13:31 +0200

    Added ScrabbleScore

--- exercises/scrabble-score/ScrabbleScore.cs
+++ exercises/scrabble-score/ScrabbleScore.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class ScrabbleScore
 {
+    private static readonly Dictionary<int, char[]> _scoreList = new Dictionary<int, char[]>()
+    {
+        [1] = new char[] { 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' },
+        [2] = new char[] { 'D', 'G' },
+        [3] = new char[] { 'B', 'C', 'M', 'P' },
+        [4] = new char[] { 'F', 'H', 'V', 'W', 'Y' },
+        [5] = new char[] { 'K' },
+        [8] = new char[] { 'J', 'X' },
+        [10] = new char[] { 'Q', 'Z' }
+    };
+
     public static int Score(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return input.ToUpperInvariant().ToCharArray().Select(c => GetScoreByCharacter(c)).Sum();
     }
+
+    private static int GetScoreByCharacter(this char character) => _scoreList
+        .SelectMany(l => l.Value.Select(s => new { letter = s, score = l.Key }))
+        .FirstOrDefault(l => l.letter == character)
+        .score;
 }
