abf2cb604fc622ef88d71a39f0ae3fb8e62f2eee
martinvds <martinvds@gmail.com>
2019-05-12 12:08:48 +0200

    Added GradeSchool

--- exercises/grade-school/GradeSchool.cs
+++ exercises/grade-school/GradeSchool.cs
@@ -1,20 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GradeSchool
 {
+    private readonly Dictionary<int, SortedSet<string>> roster = new Dictionary<int, SortedSet<string>>();
+
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!roster.ContainsKey(grade))
+        {
+            roster.Add(grade, new SortedSet<string>());
+        }
+
+        roster[grade].Add(student);
     }
 
-    public IEnumerable<string> Roster()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public IEnumerable<string> Roster() => roster.OrderBy(r => r.Key).Select(r => r.Value).SelectMany(n => n);
 
-    public IEnumerable<string> Grade(int grade)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public IEnumerable<string> Grade(int grade) => !roster.ContainsKey(grade) ? Array.Empty<string>() : roster[grade].Select(r => r);
 }
