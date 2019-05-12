54d180e083523e3b7c176060b47f4bb88a7f14c7
martinvds <martinvds@gmail.com>
2019-05-12 12:06:32 +0200

    Added Clock

--- exercises/clock/Clock.cs
+++ exercises/clock/Clock.cs
@@ -1,40 +1,46 @@
 using System;
 
-public class Clock
+public class Clock : IEquatable<Clock>
 {
+    private TimeSpan _currentTime;
+
     public Clock(int hours, int minutes)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        while (minutes < 0)
+        {
+            minutes = minutes + 60;
+            hours--;
+        }
+
+        while (hours < 0)
+        {
+            hours = hours + 24;
+        }
+
+        _currentTime = new TimeSpan(hours, minutes, 0);
     }
 
-    public int Hours
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
-
-    public int Minutes
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+    public int Hours => _currentTime.Hours;
+    public int Minutes => _currentTime.Minutes;
 
     public Clock Add(int minutesToAdd)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var changeTime = new TimeSpan(0, minutesToAdd, 0);
+
+        var newTime = _currentTime + changeTime;
+
+        return new Clock(newTime.Hours, newTime.Minutes);
     }
 
     public Clock Subtract(int minutesToSubtract)
     {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        var changeTime = new TimeSpan(0, minutesToSubtract, 0);
 
-    public override string ToString()
-    {
-        throw new NotImplementedException("You need to implement this function.");
+        var newTime = _currentTime - changeTime;
+
+        return new Clock(newTime.Hours, newTime.Minutes);
     }
+    public bool Equals(Clock other) => Hours == other.Hours && Minutes == other.Minutes;
+
+    public override string ToString() => new DateTime(_currentTime.Ticks).ToString("HH:mm");
 }
