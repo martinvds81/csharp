83fc36ff473bd80cd45424af67945a0d3de218dc
martinvds <martinvds@gmail.com>
2019-05-12 12:12:19 +0200

    Added RomanNumeral

--- exercises/roman-numerals/RomanNumerals.cs
+++ exercises/roman-numerals/RomanNumerals.cs
@@ -1,9 +1,36 @@
-using System;
+using System.Collections.Generic;
+using System.Text;
 
 public static class RomanNumeralExtension
 {
+    private static List<KeyValuePair<int, string>> numbers = new List<KeyValuePair<int, string>>()
+    {
+        new KeyValuePair<int, string>(1000, "M"),
+        new KeyValuePair<int, string>(900,"CM"),
+        new KeyValuePair<int, string>(500, "D"),
+        new KeyValuePair<int, string>(400, "CD"),
+        new KeyValuePair<int, string>(100, "C"),
+        new KeyValuePair<int, string>(90, "XC"),
+        new KeyValuePair<int, string>(50, "L"),
+        new KeyValuePair<int, string>(40,"XL"),
+        new KeyValuePair<int, string>(10, "X"),
+        new KeyValuePair<int, string>(9, "IX"),
+        new KeyValuePair<int, string>(5, "V"),
+        new KeyValuePair<int, string>(4, "IV"),
+        new KeyValuePair<int, string>(1, "I"),
+    };
+
     public static string ToRoman(this int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var builder = new StringBuilder();
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            while (value >= numbers[i].Key)
+            {
+                value -= numbers[i].Key;
+                builder.Append(numbers[i].Value);
+            }
+        }
+        return builder.ToString();
     }
 }
